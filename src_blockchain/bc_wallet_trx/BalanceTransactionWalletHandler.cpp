6eac0eb741f301d9dae4f8b73bf9c52f8474578a
iizuka <iizuka@alinous.org>
2026-08-05 20:10:17 +0800

    uncomment

--- src_blockchain/bc_wallet_trx/BalanceTransactionWalletHandler.cpp
+++ src_blockchain/bc_wallet_trx/BalanceTransactionWalletHandler.cpp
@@ -102,7 +102,6 @@ BalanceTransferTransaction* BalanceTransactionWalletHandler::createTransaction(A
 	trx->sign(musigProvidor, &utxoFinder);
 
 	return __STP_MV(trx);
-	// FIXME
 }
 
 void BalanceTransactionWalletHandler::collectUtxoRefs(BalanceTransferTransaction* trx, BalanceUnit& amount, const BalanceUnit& feeRate
@@ -143,7 +142,7 @@ void BalanceTransactionWalletHandler::collectUtxoRefs(BalanceTransferTransaction
 
 		// add exchange address
 		if(totalIn.compareTo(&required) > 0){
-			if(trx->getUtxoSize() == destUtxoSize){ // FIXME utxo size
+			if(trx->getUtxoSize() == destUtxoSize){ // utxo size
 				// add
 				ChangeAddressStore* changeAddresses = this->account->getChangeAddresses();
 				AddressDescriptor* changeDesc = changeAddresses->getNextChangeAddress(encoder); __STP(changeDesc);
